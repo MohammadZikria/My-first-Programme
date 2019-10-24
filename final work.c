5a035ac5bdd30392f31d1edeaf0699e7e6d79ecb
Muhammad Zikria <56864313+MohammadZikria@users.noreply.github.com>
2019-10-24 12:21:02 +0500

    Add files via upload

---
+++ final work.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdio.h>
+
+int main()
+{
+	int i;
+	for (i=0;i<=7;i++)
+{
+	int day;
+	printf ("Enter Number of Day\n");
+	scanf("%d",&day);
+	
+if(day==1){
+	printf("first day monday (ICT)\n");
+}
+if(day==2){
+	printf("second day tuseday (Programmming Fundamental)\n");
+}
+if(day==3){
+	printf("third day wednesday (English)\n");
+}
+if(day==4){
+	printf("fourth day thursday (Applied Physics)\n");
+}
+if(day==5){
+	printf("fifth day friday (Mathmathics)\n");
+}
+if(day==6){
+	printf("sixth day saturday (holiday)\n");
+}
+if(day==7){
+	printf("seventh day sunday (holiday)\n");
+}
+}
+	return 0;
+}
